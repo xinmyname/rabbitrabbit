7dada077b580a1055d7d388607081b55e4d846ca
Andy Sherwood <xinmyname@gmail.com>
2025-06-22 09:06:36 -0700

    Use random seed for preditable output, update README with better instructions.

--- mkprairie/Program.cs
+++ mkprairie/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System.Text;
 
-if (args.Length < 3)
+if (args.Length < 2)
 {
-    Console.WriteLine("Usage: mkprairie <dimensions> <num_rabbits> <output_file>");
+    Console.WriteLine("Usage: mkprairie <dimensions> <num_rabbits> [<output_file>]");
     Console.WriteLine();
     Console.WriteLine("    dimensions: WxH (e.g., 160x90)");
     Console.WriteLine("    num_rabbits: The number of rabbits on the prairie");
-    Console.WriteLine("    output_file: The path to the output file");
+    Console.WriteLine("    output_file: The path to the output file, or stdout if not specified");
+    Console.WriteLine();
 
     return;
 }
 
 string dimensions = args[0];
 int numRabbits = int.Parse(args[1]);
-string outputFile = args[2];
+
+Stream outputStream = (args.Length > 2)
+    ? new FileStream(args[2], FileMode.Create)
+    : Console.OpenStandardOutput();
+
+string outputFile = args.Length > 2 ? args[2] : "stdout";
 
 // Parse dimensions (format: WxH)
 var dims = dimensions.Split('x');
@@ -24,12 +30,12 @@ if (dims.Length != 2 || !int.TryParse(dims[0], out int width) || !int.TryParse(d
 }
 
 var rabbits = new HashSet<Position>();
-var rnd = new Random();
+var rnd = new Random(width * (height>>1) + numRabbits);
 
 while (rabbits.Count < numRabbits)
     rabbits.Add(new Position(rnd.Next(0, width), rnd.Next(0, height)));
 
-using (var writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+using (var writer = new StreamWriter(outputStream, Encoding.UTF8))
 {
     writer.NewLine = "\n"; // Force UNIX line endings
     writer.WriteLine($"{width}x{height}");
@@ -48,11 +54,15 @@ using (var writer = new StreamWriter(outputFile, false, Encoding.UTF8))
         }
 
         writer.WriteLine(lineBuilder.ToString());
-        Console.Write($"\rLine {y+1}...");
+        if (outputStream is FileStream)
+            Console.Write($"\rLine {y+1}...");
     }
 }
 
-Console.WriteLine("Done!");
+if (outputStream is FileStream)
+    Console.WriteLine();
+
+outputStream.Close();
 
 static string NextGroundPatch()
 {
